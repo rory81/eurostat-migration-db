90d087b7f1b78829ab7eb129d0c9c43e0d86d046
EC2 Default User <ec2-user@ip-172-31-82-249.ec2.internal>
2019-08-22 18:49:44 +0000

    load data for access D3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,7 @@
-const baseURL = "https://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/migr_asyappctza?citizen=EU28&filterNonGeo=1&precision=1&sex=T&asyl_app=ASY_APP&geo=TOTAL&unit=PER&unitLabel=label&shortLabel=1&age=TOTAL";
-
-function getData(cb) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
-
-    xhr.open("GET", baseURL);
-    xhr.send();
-}
-
-function printDataToConsole(data) {
-    console.log(data.value);
-}
-
-// getData(printDataToConsole);
+queue()
+    .defer(d3.json,"https://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/migr_asyappctza?citizen=EU28&filterNonGeo=1&precision=1&sex=T&asyl_app=ASY_APP&geo=TOTAL&unit=PER&unitLabel=label&shortLabel=1&age=TOTAL")
+    .await(makeGraphs);
+    
+    function makeGraphs(error,migrData){
+        
+    }
