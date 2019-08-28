8ded51b7c9a1c94d366edbde3afccb13a0b7394a
EC2 Default User <ec2-user@ip-172-31-82-249.ec2.internal>
2019-08-28 17:30:34 +0000

    add total graph

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,25 @@
 queue()
-    .defer(d3.json,"https://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/migr_asyappctza?citizen=EU28&filterNonGeo=1&precision=1&sex=T&asyl_app=ASY_APP&geo=TOTAL&unit=PER&unitLabel=label&shortLabel=1&age=TOTAL")
+    .defer(d3.csv,"assets/data/migrationdata")
     .await(makeGraphs);
     
     function makeGraphs(error,migrData){
+        
+        var ndx = crossfilter(migrData);
+        var time_dim = ndx.dimension(dc.pluck('TIME'));
+        var total_asylum_applications_per_year = time_dim.group().reduceSum(dc.pluck('Value'));
+        
+        dc.barChart('#total_asylum_applications_per_year')
+            .width(1000)
+            .height(500)
+            .margins({top:10, right:50, bottom:30, left:50})
+            .dimension(time_dim)
+            .group(total_asylum_applications_per_year)
+            .transitionDuration(500)
+            .x(d3.scale.ordinal())
+            .xUnits(dc.units.ordinal)
+            .yAxisLabel("# asylum applicants")
+            .yAxis().ticks(5);
+        
+        dc.renderAll();
         
     }
