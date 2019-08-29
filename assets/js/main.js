6fd291fce00603e6c50a6273c3a221c6cdab8405
EC2 Default User <ec2-user@ip-172-31-82-249.ec2.internal>
2019-08-29 09:49:10 +0000

    centering total_graph and format yaxis label and color bars

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,26 @@
 queue()
-    .defer(d3.csv,"assets/data/migrationdata")
+    .defer(d3.csv, "assets/data/migrationdata")
     .await(makeGraphs);
-    
-    function makeGraphs(error,migrData){
-        
-        var ndx = crossfilter(migrData);
-        var time_dim = ndx.dimension(dc.pluck('TIME'));
-        var total_asylum_applications_per_year = time_dim.group().reduceSum(dc.pluck('Value'));
-        
-        dc.barChart('#total_asylum_applications_per_year')
-            .width(1000)
-            .height(500)
-            .margins({top:10, right:50, bottom:30, left:50})
-            .dimension(time_dim)
-            .group(total_asylum_applications_per_year)
-            .transitionDuration(500)
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .yAxisLabel("# asylum applicants")
-            .yAxis().ticks(5);
-        
-        dc.renderAll();
-        
-    }
+
+function makeGraphs(error, migrData) {
+
+    var ndx = crossfilter(migrData);
+    var time_dim = ndx.dimension(dc.pluck('TIME'));
+    var total_asylum_applications_per_year = time_dim.group().reduceSum(dc.pluck('Value'));
+
+    dc.barChart('#total_asylum_applications_per_year')
+        .width(1000)
+        .height(500)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(time_dim)
+        .group(total_asylum_applications_per_year)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .ordinalColors(["#FF6600"])
+        .yAxisLabel("# asylum applicants")
+        .yAxis().ticks(5);
+
+    dc.renderAll();
+
+}
