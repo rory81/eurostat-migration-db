056f72d3cfa67e473317d99a08289555aa5f9bea
EC2 Default User <ec2-user@ip-172-31-82-249.ec2.internal>
2019-09-01 15:04:46 +0000

    add country column and parsed value to int and made graph responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,23 @@
 queue()
-    .defer(d3.csv, "assets/data/migrationdata")
+    .defer(d3.csv, "assets/data/migrationData.csv")
     .await(makeGraphs);
 
 function makeGraphs(error, migrData) {
-
+    
+    migrData.forEach(function(d){
+        d.Value = parseInt(d["Value"])
+    })
+    
     var ndx = crossfilter(migrData);
     var time_dim = ndx.dimension(dc.pluck('TIME'));
     var total_asylum_applications_per_year = time_dim.group().reduceSum(dc.pluck('Value'));
+    
+    // var country_dim =ndx.dimension(dc.pluck())
 
     dc.barChart('#total_asylum_applications_per_year')
         .width(300)
         .height(150)
+        .useViewBoxResizing(true)
         .margins({ top: 10, right: 30, bottom: 30, left: 30 })
         .dimension(time_dim)
         .group(total_asylum_applications_per_year)
@@ -20,6 +27,8 @@ function makeGraphs(error, migrData) {
         .ordinalColors(["#FF6600"])
         .yAxisLabel("# asylum applicants")
         .yAxis().ticks(5);
+        
+    
 
     dc.renderAll();
 
