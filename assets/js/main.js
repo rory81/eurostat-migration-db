106efee08ea54877329c20fa2123e39053996443
EC2 Default User <ec2-user@ip-172-31-82-249.ec2.internal>
2019-09-16 19:53:42 +0000

    removed width and heigth from scripts and add those to css

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,7 @@ function makeGraphs(error, migrData) {
     migrData.forEach(function(d) {
         d.Value = d["Value"].replace(/,/g, '');
         d.Value = parseInt(d["Value"]);
+        // str.replace(/blue/g, "red")
     })
 
     show_barChart(ndx);
@@ -34,15 +35,11 @@ function show_barChart(ndx) {
     // 2018	665250
 
     dc.barChart('#total_asylum_applications_per_year')
-        .width(300)
-        .height(150)
+
         .useViewBoxResizing(true)
         .margins({ top: 10, right: 30, bottom: 30, left: 30 })
         .dimension(dim)
         .group(sumApplications)
-        // .valueAccessor(function(d) {
-        //     return d.value.total;
-        // })
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
@@ -57,32 +54,22 @@ function show_country_pie(ndx) {
     var top5CountryPie = dim.group().reduceSum(dc.pluck('Value'));
 
     dc.pieChart('#top-5-pie')
-        .width(330)
-        .radius(90)
         .useViewBoxResizing(true)
         .dimension(dim)
         .group(top5CountryPie)
         .transitionDuration(1500)
         .cap(5)
-        .legend(dc.legend().x(270).y(0).gap(5))
+        .legend(dc.legend().x(2).y(0).gap(5))
         .renderLabel(false);
 }
-
-
 
 
 function show_country_table(ndx) {
     var dataTable = dc.dataTable("#top-5");
 
-
     //dimension
     var countryDim = ndx.dimension(function(d) { return d.GEO; });
-
-    //group
-    var orderByWD = countryDim.group().reduceSum(function(d) { return d.Value; });
-
-    // Actually a dimension keyed on week and site
-    var fakeDateDim = {
+    var fakeCountryDim = {
         top: function(d) {
             var m = dc.d3.map();
             countryDim.top(Infinity).forEach(function(g) {
@@ -95,7 +82,9 @@ function show_country_table(ndx) {
                         GEO: g.GEO,
                         Value: g.Value
                     });
+                    m.values();
                 }
+
             });
 
             return m.values();
@@ -103,21 +92,19 @@ function show_country_table(ndx) {
     };
 
 
-    dataTable.width(800).height(800)
-        .dimension(fakeDateDim)
+    dataTable
+        .dimension(fakeCountryDim)
         .group(function(d) { return '' })
-        .size(5)
+        .showGroups(false)
         .columns([
-            // function (d) { var format = d3.format('02d');
-            //    return d.date.getFullYear() +'/'+ format(d.date.getMonth()+1) + '/' + format(d.date.getDate()); },
-            //function (d) { return d.week; },
             function(d) { return d.GEO; },
             function(d) { return d.Value; }
         ])
         .sortBy(function(d) { return d.Value; })
-        .order(d3.descending);
+        .order(d3.descending)
+        .size([1,5]);
 
-    dataTable.on('renderlet', function(chart) {
-        chart.selectAll('.dc-table-group').classed('info', true);
-    });
+    // dataTable.on('renderlet', function(chart) {
+    //     chart.selectAll('.dc-table-group').classed('info', true);
+    // });
 }
