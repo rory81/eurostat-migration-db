0b75d060f959905e75d24541b7172ae187c22b86
EC2 Default User <ec2-user@ip-172-31-82-249.ec2.internal>
2019-09-21 14:51:28 +0000

    changed order AGE and made totalGraph sticky-top

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,14 @@ function makeGraphs(error, migrData) {
 
     migrData.forEach(function(d) {
         d.Value = d["Value"].replace(/,/g, '');
-        d.Value = parseInt(d["Value"]);
-    })
+        d.Value = +d["Value"]; //coerce to number
+
+        d.AGE = d["AGE"].replace(/From /g, 'Between ')
+        d.AGE = d["AGE"].replace(/Less than /g, 'Age under ')
+        d.AGE = d["AGE"].replace(/Unknown/g, ' Unknown')
+        d.AGE = d["AGE"].replace(/65 years or over/g, 'Over 64 years')
+
+    });
 
     show_barChart(ndx);
     show_country_table(ndx);
@@ -40,16 +46,12 @@ function show_barChart(ndx) {
         .margins({ top: 10, right: 30, bottom: 30, left: 30 })
         .dimension(dim)
         .group(sumApplications)
-        // .valueAccessor(function(d) {
-        //     return d.value.total;
-        // })
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .ordinalColors(["#FF6600"])
         .yAxisLabel("# asylum applicants")
-        .yAxis().ticks(5).tickFormat(function(d) { return d / 1000000 + " M" });
-
+        .yAxis().ticks(5).tickFormat(d3.format(".2s"));
 }
 
 function show_country_pie(ndx) {
@@ -162,55 +164,12 @@ function show_sex_table(ndx) {
         .sortBy(function(d) { return d.value.total; })
         .order(d3.descending);
 }
-function show_country_table(ndx) {
-    var dataTable = dc.dataTable("#top-5");
-    var countryDim = ndx.dimension(function(d) { return d.GEO; });
-    var valGrpdDim = countryDim.group().reduce(
-
-        function(p, v) {
-
-            ++p.number;
-
-            p.total += +v.Value;
-
-            return p;
-
-        },
-
-        function(p, v) {
-
-            --p.number;
-
-            p.total -= +v.Value;
-
-            return p;
-
-        },
-
-        function() {
-
-            // console.log('initias entered')
-
-            return { number: 0, total: 0 }
-
-        });
-
-    valGrpdDim.order(v => v.total);
-
-    dataTable.width(800).height(800)
-
-        .dimension(valGrpdDim)
-        .group(function(d) { return 'Country | Value' })
-        .showGroups(false)
-        .size(5)
-        .columns([function(d) { return d.key }, function(d) { return d.value.total }])
-        .sortBy(function(d) { return d.value.total; })
-        .order(d3.descending);
-}
 
 function show_age_table(ndx) {
     var dataTable = dc.dataTable("#ageTable");
     var ageDim = ndx.dimension(function(d) { return d.AGE; });
+
+
     var valGrpdDim = ageDim.group().reduce(
 
         function(p, v) {
@@ -250,6 +209,6 @@ function show_age_table(ndx) {
         .showGroups(false)
         .size(5)
         .columns([function(d) { return d.key }, function(d) { return d.value.total }])
-        .sortBy(function(d) { return d.AGE; })
+        .sortBy(function(d) { return d.age; })
         .order(d3.descending);
 }
