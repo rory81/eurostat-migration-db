04c10517f2b35cf08a859c270a206f20acc1df59
EC2 Default User <ec2-user@ip-172-31-82-249.ec2.internal>
2019-09-28 15:28:07 +0000

    did some cleanup code based on some JS validator

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,13 +8,13 @@ function makeGraphs(error, migrData) {
 
     migrData.forEach(function(d) {
 
-        d.Value = d["Value"].replace(/,/g, '');
-        d.Value = +d["Value"]; //coerce to number
+        d.Value = d.Value.replace(/,/g, "");
+        d.Value = +d.Value; //coerce to number
 
-        d.AGE = d["AGE"].replace(/From /g, 'Between ')
-        d.AGE = d["AGE"].replace(/Less than /g, 'Age under ')
-        d.AGE = d["AGE"].replace(/Unknown/g, ' Unknown')
-        d.AGE = d["AGE"].replace(/65 years or over/g, 'Over 64 years')
+        d.AGE = d.AGE.replace(/From /g, "Between ")
+        d.AGE = d.AGE.replace(/Less than /g, "Age under ")
+        d.AGE = d.AGE.replace(/Unknown/g, " Unknown")
+        d.AGE = d.AGE.replace(/65 years or over/g, "Over 64 years")
 
     });
 
@@ -30,8 +30,8 @@ function makeGraphs(error, migrData) {
 }
 
 function show_barChart(ndx) {
-    var dim = ndx.dimension(dc.pluck('TIME'));
-    var sumApplications = dim.group().reduceSum(dc.pluck('Value'));
+    var dim = ndx.dimension(dc.pluck("TIME"));
+    var sumApplications = dim.group().reduceSum(dc.pluck("Value"));
 
     // Total per year according to data
     // 2013	457630
@@ -41,7 +41,7 @@ function show_barChart(ndx) {
     // 2017	735410
     // 2018	665250
 
-    dc.barChart('#total_asylum_applications_per_year')
+    dc.barChart("#total_asylum_applications_per_year")
         .width(300)
         .height(150)
         .useViewBoxResizing(true)
@@ -57,10 +57,10 @@ function show_barChart(ndx) {
 }
 
 function show_country_pie(ndx) {
-    var dim = ndx.dimension(dc.pluck('GEO'));
-    var top5CountryPie = dim.group().reduceSum(dc.pluck('Value'));
+    var dim = ndx.dimension(dc.pluck("GEO"));
+    var top5CountryPie = dim.group().reduceSum(dc.pluck("Value"));
 
-    dc.pieChart('#top-5-pie')
+    dc.pieChart("#top-5-pie")
         .width(330)
         .height(180)
         .radius(90)
@@ -79,7 +79,10 @@ function show_country_pie(ndx) {
 function show_country_table(ndx) {
     var dataTable = dc.dataTable("#top-5");
     var countryDim = ndx.dimension(function(d) { return d.GEO; });
+    var dateDim = ndx.dimension(function(d) { return d.TIME; });
+    // var grandTotal = d3.sum(dc.pluck("Value"));
     var grandTotal = 5208960;
+
     var valGrpdDim = countryDim.group().reduce(
 
         function(p, v) {
@@ -104,18 +107,18 @@ function show_country_table(ndx) {
 
         function() {
 
-            // console.log('initias entered')
-
-            return { number: 0, total: 0 }
+            return { number: 0, total: 0 };
 
         });
 
     valGrpdDim.order(v => v.total);
 
+
+
     dataTable.width(800).height(800)
 
         .dimension(valGrpdDim)
-        .group(function(d) { return 'Country | Value' })
+        .group(function(d) { return "Country | Value" })
         .showGroups(false)
         .size(5)
         .columns([function(d) { return d.key }, function(d) { return d.value.total }, function(d) { return ((d.value.total / grandTotal) * 100).toFixed(2) + "%" }])
@@ -127,11 +130,6 @@ function show_sex_table(ndx) {
     var dataTable = dc.dataTable("#sexTable");
     var sexDim = ndx.dimension(function(d) { return d.SEX; });
     var grandTotal = 5208960;
-
-    // var pictures = [];
-    // pictures[0] = "/assets/images/male.png";
-    // pictures[1] = "/assets/images/female2.png";
-    // pictures[2] = "/assets/images/questionmark.png";
 
     var valGrpdDim = sexDim.group().reduce(
 
@@ -157,8 +155,6 @@ function show_sex_table(ndx) {
 
         function() {
 
-            // console.log('initias entered')
-
             return { number: 0, total: 0 }
 
         });
@@ -168,46 +164,13 @@ function show_sex_table(ndx) {
     dataTable.width(800).height(800)
 
         .dimension(valGrpdDim)
-        .group(function(d) { return 'Sex | Value' })
+        .group(function(d) { return "Sex | Value" })
         .showGroups(false)
         .size(5)
-        .columns(
-            // [function(d) {
-        //     if (d.key === 'Males') {
-        //         var images = document.createElement("img");
-        //         images.setAttribute('src', '/assets/images/male.png');
-
-        //         return images
-
-        //     }
-        //     else if (d.key === 'Females') {
-        //         return "Picture Female";
-        //     }
-        //     else {
-        //         return "Picture questionmark";
-        //     }
-        // }, 
-        [function(d){return d.key},function(d) { return d.value.total }, function(d) { return ((d.value.total / grandTotal) * 100).toFixed(2) + "%" }])
+        .columns([function(d) { return d.key }, function(d) { return d.value.total }, function(d) { return ((d.value.total / grandTotal) * 100).toFixed(2) + "%" }])
         .sortBy(function(d) { return d.value.total; })
         .order(d3.descending);
 
-
-
-    // function(d) {
-    //     if (d.SEX === 'Males') {
-
-    //         d.SEX = "/assets/images/male.png";
-    //         return d.SEX;
-    //     }
-    //     else if (d.SEX === 'Females') {
-    //         d.SEX = "/assets/images/female2.png";
-    //         return d.SEX;
-    //     }
-    //     else {
-    //         d.SEX = "/assets/images/questionmark.png";
-    //         return d.SEX;
-    //     }
-    // }
 }
 
 function show_age_table(ndx) {
@@ -240,8 +203,6 @@ function show_age_table(ndx) {
 
         function() {
 
-            // console.log('initias entered')
-
             return { number: 0, total: 0 }
 
         });
@@ -251,7 +212,7 @@ function show_age_table(ndx) {
     dataTable.width(800).height(800)
 
         .dimension(valGrpdDim)
-        .group(function(d) { return 'Age | Value' })
+        .group(function(d) { return "Age | Value" })
         .showGroups(false)
         .size(5)
         .columns([function(d) { return d.key }, function(d) { return d.value.total }, function(d) { return ((d.value.total / grandTotal) * 100).toFixed(2) + "%" }])
